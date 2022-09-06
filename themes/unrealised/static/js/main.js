119c60ff59118b35efdaa3464e6ab4807d451cf4
axkeyz <xahkun@gmail.com>
2022-09-06 18:09:26 +1200

    Update navigation menu

---
+++ themes/unrealised/static/js/main.js
@@ -0,0 +1,8 @@
+function navigation() {
+  var x = document.getElementById("navbar");
+  if (x.className === "nav-wrapper") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-wrapper";
+  }
+}
