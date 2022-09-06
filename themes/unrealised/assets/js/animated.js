d9532ab8cfb796cc593f92322e89a6e87b19feee
axkeyz <xahkun@gmail.com>
2022-09-07 00:20:56 +1200

    Minify resources

--- themes/unrealised/assets/js/animated.js
+++
@@ -1,3 +0,0 @@
-// gsap.to("#phone-group, #phone-dot-group", { x:20, duration: 1});
-gsap.to("#phone-dot-group", { rotate: -500, y:575, x:-220, delay: 2, transformOrigin:"bottom right 50%", duration: 6});
-gsap.to("#phone-group", { x:7, delay: 8.5, duration: 2});
