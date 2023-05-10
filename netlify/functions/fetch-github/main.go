b5c5a712f813fe72eb6cc05dc709664dca4198ab
axkeyz <xahkun@gmail.com>
2023-05-10 22:32:54 +1200

    Restore netlify-github function

--- netlify/functions/fetch-github/main.go
+++ netlify/functions/fetch-github/main.go
@@ -1,14 +1,37 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	response, err := http.Get("http://pokeapi.co/api/v2/pokedex/kanto/")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(responseData))
+
 	return &events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello, World!",
+		StatusCode:      200,
+		Headers:         map[string]string{"Content-Type": "text/plain"},
+		Body:            responseData,
+		IsBase64Encoded: false,
 	}, nil
 }
 
